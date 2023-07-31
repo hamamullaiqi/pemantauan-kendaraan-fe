d919325d26ab2ead73a7fdd2d3bdab2f6a88a18a
andi lauw <andi.safariansyah7@gmail.com>
2023-08-01 03:03:12 +0700

    merge request

--- src/contents/apps/pages/menus.js
+++ src/contents/apps/pages/menus.js
@@ -1,8 +1,8 @@
 import {
-    HiClipboardList,
-    HiHome,
-    HiOutlineDocument,
-    HiClipboard,
+  HiClipboardList,
+  HiHome,
+  HiOutlineDocument,
+  HiClipboard,
 } from "react-icons/hi";
 import { GoDotFill } from "react-icons/go";
 import { Link } from "react-router-dom";
@@ -10,92 +10,88 @@ import { Link } from "react-router-dom";
 const SIZE = 18;
 
 const Menus = [
-    {
-        key: "/apps/home",
-        icon: <HiHome size={SIZE} />,
-        level: 0x1fff,
+  {
+    key: "/apps/home",
+    icon: <HiHome size={SIZE} />,
+    level: 0x1fff,
+    role: ["ADM"],
+    label: <Link to={"/apps/home"}>Home</Link>,
+  },
+  {
+    key: "/apps/master",
+    icon: <HiClipboardList size={SIZE} />,
+    level: 0x1ff0,
+    role: ["ADM", "MNG"],
+
+    children: [
+      {
+        key: "/apps/master/masuk",
+        icon: <GoDotFill size={SIZE} />,
+        level: 0x1ff0,
+        label: <Link to={"/apps/master/masuk"}>Kendaraan Masuk</Link>,
         role: ["ADM"],
-        label: <Link to={"/apps/home"}>Home</Link>,
-    },
-    {
-        key: "/apps/master",
-        icon: <HiClipboardList size={SIZE} />,
+      },
+      {
+        key: "/apps/master/keluar",
+        icon: <GoDotFill size={SIZE} />,
+        level: 0x1ff0,
+        label: <Link to={"/apps/master/keluar"}>Kendaraan Keluar</Link>,
+        role: ["ADM"],
+      },
+      {
+        key: "/apps/master/produk",
+        icon: <GoDotFill size={SIZE} />,
+        level: 0x1ff0,
+        label: <Link to={"/apps/master/produk"}>Produk</Link>,
+        role: ["ADM"],
+      },
+      {
+        key: "/apps/master/vendor",
+        icon: <GoDotFill size={SIZE} />,
+        level: 0x1ff0,
+        label: <Link to={"/apps/master/vendor"}>Vendor</Link>,
+        role: ["ADM"],
+      },
+      {
+        key: "/apps/master/users",
+        icon: <GoDotFill size={SIZE} />,
+        level: 0x1ff0,
+        label: <Link to={"/apps/master/users"}>Users</Link>,
+        role: ["MNG"],
+      },
+    ],
+    label: <Link to={"#"}>Master</Link>,
+  },
+  {
+    key: "/apps/laporan",
+    icon: <HiClipboard size={SIZE} />,
+    level: 0x1ff0,
+    role: ["ADM", "MNG"],
+
+    children: [
+      {
+        key: "/apps/laporan/kendaraan-masuk",
+        icon: <GoDotFill size={SIZE} />,
         level: 0x1ff0,
         role: ["ADM", "MNG"],
 
-        children: [
-            {
-                key: "/apps/master/masuk",
-                icon: <GoDotFill size={SIZE} />,
-                level: 0x1ff0,
-                label: <Link to={"/apps/master/masuk"}>Kendaraan Masuk</Link>,
-                role: ["ADM"],
-            },
-            {
-                key: "/apps/master/keluar",
-                icon: <GoDotFill size={SIZE} />,
-                level: 0x1ff0,
-                label: <Link to={"/apps/master/keluar"}>Kendaraan Keluar</Link>,
-                role: ["ADM"],
-            },
-            {
-                key: "/apps/master/produk",
-                icon: <GoDotFill size={SIZE} />,
-                level: 0x1ff0,
-                label: <Link to={"/apps/master/produk"}>Produk</Link>,
-                role: ["ADM"],
-            },
-            {
-                key: "/apps/master/vendor",
-                icon: <GoDotFill size={SIZE} />,
-                level: 0x1ff0,
-                label: <Link to={"/apps/master/vendor"}>Vendor</Link>,
-                role: ["ADM"],
-            },
-            {
-                key: "/apps/master/users",
-                icon: <GoDotFill size={SIZE} />,
-                level: 0x1ff0,
-                label: <Link to={"/apps/master/users"}>Users</Link>,
-                role: ["MNG"],
-            },
-        ],
-        label: <Link to={"#"}>Master</Link>,
-    },
-    {
-        key: "/apps/laporan",
-        icon: <HiClipboard size={SIZE} />,
+        label: (
+          <Link to={"/apps/laporan/kendaraan-masuk"}>Kendaraan Masuk</Link>
+        ),
+      },
+      {
+        key: "/apps/laporan/kendaraan-keluar",
+        icon: <GoDotFill size={SIZE} />,
         level: 0x1ff0,
-        role: ["MNG"],
+        role: ["ADM", "MNG"],
 
-        children: [
-            {
-                key: "/apps/laporan/kendaraan-masuk",
-                icon: <GoDotFill size={SIZE} />,
-                level: 0x1ff0,
-                role: ["MNG"],
-
-                label: (
-                    <Link to={"/apps/laporan/kendaraan-masuk"}>
-                        Kendaraan Masuk
-                    </Link>
-                ),
-            },
-            {
-                key: "/apps/laporan/kendaraan-keluar",
-                icon: <GoDotFill size={SIZE} />,
-                level: 0x1ff0,
-                role: ["MNG"],
-
-                label: (
-                    <Link to={"/apps/laporan/kendaraan-keluar"}>
-                        Kendaraan Keluar
-                    </Link>
-                ),
-            },
-        ],
-        label: <Link to={"#"}>Laporan</Link>,
-    },
+        label: (
+          <Link to={"/apps/laporan/kendaraan-keluar"}>Kendaraan Keluar</Link>
+        ),
+      },
+    ],
+    label: <Link to={"#"}>Laporan</Link>,
+  },
 ];
 
 export default Menus;
